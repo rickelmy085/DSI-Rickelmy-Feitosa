935d5ff9a591bc6aac085af9addf252b8c1caf89
rickelmy085 <rfeitosamalafaia@gmail.com>
2026-05-04 19:23:22 -0300

    Adicionei o exercico usando os operadores && || e !

--- aula06/Logica_AndOrNot.java
+++ aula06/Logica_AndOrNot.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 
 public class Logica_AndOrNot {
     public static void main(String[] args) {
+        //Espere que fique feliz professor, vc vai ficar muito feliz com essa sequência de aura
+        //Eu vi como que fazia o input no java no proprio W3School 
+
         Scanner nomeAura = new Scanner(System.in);
         System.out.println("Digite o nome da pessoa mais aura");
+        String nomeString = nomeAura.nextLine();
 
         Scanner number = new Scanner(System.in);
         System.out.println("Número aura: ");
         int newNumber = number.nextInt();
 
-        if (nomeAura == "Rodrigo" && newNumber == 67) {
-            System.out.println("Esse cara ama muito azul e tem aura boa");
+        if (nomeString.equals("Rodrigo") && newNumber == 67) {
+            System.out.println("Esse cara ama muito a linha azul e tem muita aura 67!");
+        } else if (nomeString.equals("Rickelmy") && newNumber == 67) {
+            System.out.println("A pessoa que mais tem aura da face da terra!!!");
+        } else if (nomeString.equals("") || newNumber == 67) {
+            System.out.println("Ele(a) "+ nomeString + " não tem muita aura, mas 67!!");
+        } else if (nomeString.equals("") && newNumber == 67) {
+            System.out.println("Para cara, 42 nunca será o número mais aura");
+        } else {
+            System.out.println("Conseguiu errar o nome e o número, apenas seja moggado");
         }
-
-        
-    
     }
 }
